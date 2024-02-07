e143f487181e37026523c0d307dc0f372e4ad3dc
GradusFrontend <ruslandjavadov998@gmail.com>
2024-02-07 20:07:23 +0500

    add js

--- js/script.js
+++ js/script.js
@@ -3,10 +3,11 @@ const form = document.forms.todoForm
 const input = document.querySelector('#inp')
 
 let arr = []
+let wrap = document.querySelector('.wrap')
 
 form.onsubmit = (event) => {
     event.preventDefault()
-    submit()
+    if (input.value.length > 0) submit()
 }
 
 function submit() {
@@ -17,9 +18,42 @@ function submit() {
     let todoData = {
         id: Math.floor(Math.random() * 100),
         time: hours + ':' + minutes,
-        isFinished: false
+        completed: false
     }
     let formData = new FormData(form).forEach(value => todoData.title = value)
-    
-    arr.push(todoData)
+
+    let isDuplicate = arr.some(item => item.title === todoData.title);
+
+    if (!isDuplicate) {
+        arr.push(todoData);
+    }
+
+    reloadTodo(arr, wrap)
+}
+
+function reloadTodo(arr, place) {
+    place.innerHTML = ''
+
+    for (let item of arr) {
+        let todo = document.createElement('div')
+        let todoText = document.createElement('h3')
+        let timeText = document.createElement('p')
+        let deleteBtn = document.createElement('img')
+
+        todo.classList.add('todo')
+        todoText.classList.add('todo-text')
+        timeText.classList.add('time')
+        deleteBtn.classList.add('delete')
+
+        place.append(todo)
+        todo.append(todoText, timeText, deleteBtn)
+        todoText.innerHTML = item.title
+        timeText.innerHTML = item.time
+        deleteBtn.src = './cross.svg'
+
+        deleteBtn.onclick = () => {
+            todo.remove()
+            arr.splice(arr.indexOf(item), 1)
+        }
+    }
 }
