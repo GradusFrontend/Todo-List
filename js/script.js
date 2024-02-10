89c9232d0df5da3657d37495cc61120a14735f86
GradusFrontend <ruslandjavadov998@gmail.com>
2024-02-10 18:05:55 +0500

    add changes

--- js/script.js
+++ js/script.js
@@ -55,5 +55,17 @@ function reloadTodo(arr, place) {
             todo.remove()
             arr.splice(arr.indexOf(item), 1)
         }
+
+        todo.ondblclick = () => {
+            let changeTitle = prompt('ghbs')
+            item.title = changeTitle
+            todoText.innerHTML = item.title
+        }
+        todoText.onclick = () => {
+            todoText.style.textDecorationLine = 'line-through'
+            todoText.style.textDecorationColor = 'blue'
+            todoText.style.textDecorationThickness = '2px'
+            item.completed = true
+        }
     }
 }
